d55207f95775a38772e233070cf081d45e6ba9ed
VladimirGrushin <klasniy.chuvachok@gmail.com>
2025-09-07 17:33:23 +0300

    made UserController

--- src/main/java/com/example/bankcards/dto/response/UserResponse.java
+++ src/main/java/com/example/bankcards/dto/response/UserResponse.java
@@ -1,6 +1,7 @@
 package com.example.bankcards.dto.response;
 
 import com.example.bankcards.entity.Role;
+import com.example.bankcards.entity.User;
 import lombok.Data;
 
 @Data
@@ -9,4 +10,11 @@ public class UserResponse {
     private String firstName;
     private String lastName;
     private Role role;
+
+    public UserResponse(User user) {
+        this.id = user.getId();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.role = user.getRole();
+    }
 }
